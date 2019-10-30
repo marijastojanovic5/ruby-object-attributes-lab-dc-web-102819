c7b2ed3b98a21c76fc2de3db6e3cda0f184497d5
Marija Stojanovic <marijastojanovic5@yahoo.com>
2019-10-30 02:19:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,13 @@
 
   class Dog
  
-  def name=(dogs_name)
-    @name = dogs_name
-  end
- 
   def name
     @name
+   end
+   
+   def name=(new_name)
+     @name = new_name
   end
-end
- 
 
    
    
